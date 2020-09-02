16fb1d34a48487d50d7dedc5459f69122b51ad80
Wayne <waynejeng@aetheras.io>
2020-09-02 15:59:11 +0800

    [Refactor] Update venv setup script

--- virtualenv/venv_setup.sh
+++ virtualenv/venv_setup.sh
@@ -1,36 +1,76 @@
 #!/bin/sh
 
 # Variables
-venv="env"
-req_file="requirements.txt"
+VENV="env"
+REQ="requirements.txt"
+
+main () {
+	venv_create
+	venv_activate
+}
 
 # Create virtual environment
-venv_create()
-{
-	if ! [ -d "$venv" ]; then
-		python3 -m venv $venv
-		echo "virtual environment created"
+venv_create() {
+	if ! check_exist "${VENV}"; then
+		python3 -m venv ${VENV}
+		info "Virtual environment created"
 	else
-		echo "virtual environment existed"
+		info "Virtual environment existed"
 	fi
 }
 
 # Activate virtual environment
-venv_activate()
-{
-	if [ -d "$venv" ]; then
-		source $venv/bin/activate
-		echo "virtual environment activated"
+venv_activate() {
+	if check_exist "${VENV}"; then
+		source ${VENV}/bin/activate
+		info "Virtual environment activated"
 	else
-		echo "Unable to find virtual environment"
+		info "Unable to find virtual environment"
 		exit 1
 	fi
 
 	# Install packages by requirements file
-	if [ -e "$req_file" ]; then
-		pip install -r $req_file
+	if check_exist "${REQ}"; then
+		pip3 install -r ${REQ}
+		info "Packages installed"
 	fi
 }
 
-venv_create
-venv_activate
+#
+# Utils
+#
+
+# Colors
+CLEAR='\033[2K'
+NC='\033[0m'
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+info() {
+	printf "\r  [ ${BLUE}..${NC} ] $1\n"
+}
+
+ok() {
+	printf "\r${CLEAR}  [ ${GREEN}OK${NC} ] $1\n"
+}
+
+err() {
+	printf "\r${CLEAR}  [ ${RED}ERR${NC} ] $1\n"
+	exit
+}
+
+check_cmd() {
+	command -v "$1" >/dev/null 2>&1
+}
+
+check_exist() {
+	command ls "$1" >/dev/null 2>&1
+}
+
+main "$@"
