708774faf34a32000231d435c323f58f1deaaaf2
mayone <mayone321@gmail.com>
2021-04-29 18:08:05 +0800

    [Feature] Add CSV reader

---
+++ csv-reader/README.md
@@ -0,0 +1,20 @@
+# CSV Reader
+- Example of how to read/write csv file
+- Read input file and generate two files
+
+## Input
+- `input.csv`
+    - Name
+    - Height (cm)
+    - Weight (kg)
+
+## Output
+- `fit.csv`
+    - people with BMI < 25
+- `fat.csv`
+    - people with BMI >= 25
+
+## Run
+```sh
+python main.py
+```
---
+++ csv-reader/main.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+import csv
+
+if __name__ == "__main__":
+    in_file = open("input.csv", "r")
+    fit_out_file = open("fit.csv", "w")
+    fat_out_file = open("fat.csv", "w")
+
+    fit_writer = csv.writer(fit_out_file)
+    fat_writer = csv.writer(fat_out_file)
+
+    lines = csv.reader(in_file, delimiter=',')
+
+    overweight_bmi = 25
+
+    for row in lines:
+        # Write header
+        if "Name" in row[0]:
+            fit_writer.writerow(row)
+            fat_writer.writerow(row)
+            continue
+
+        [name, height_cm, weight] = row
+        height_m = float(height_cm) / 100
+        weight = float(weight)
+        bmi = weight / (height_m * height_m)
+        print(f"{name}\t{bmi}")
+
+        if bmi < overweight_bmi:
+            fit_writer.writerow(row)
+        else:
+             fat_writer.writerow(row)
+
+    in_file.close()
+    fit_out_file.close()
+    fat_out_file.close()
