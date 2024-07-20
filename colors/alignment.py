e6d7117e4449438cca4cb5ace78cc0196f8c7be5
mayone <mayone321@gmail.com>
2024-07-20 23:58:12 +0800

    [Change] Apply alignment fix

--- colors/alignment.py
+++ colors/alignment.py
@@ -103,6 +103,25 @@ def get_width(string):
     >>> get_width("Hello")
     5
 
+    >>> get_width("❤️")
+    1
+
+    >>> get_width("✊")
+    2
+
+    >>> get_width("✊🏾")
+    4
+
+    >>> get_width("🏳")
+    1
+
+    >>> get_width("🌈")
+    2
+
+    # #TODO: Fix this test
+    # >>> get_width("🏳️‍🌈")
+    # 3
+
     >>> get_width("")
     0
     """
@@ -113,13 +132,21 @@ def get_width(string):
     string = remove_ansi_escape(string)
 
     width = 0
-    # combining_char = u'[?([\u0300-\u036F]'
+    # combining_char = "[?([\u0300-\u036f]"
+    variation_selector = "[?([\ufe00-\ufe0f]"
+
     for i in range(len(string)):
         # Combining character
         # if re.match(combining_char, string[i]):
         if unicodedata.combining(string[i]):
             if i == 0:
-                # If combining character in beginning of the line alone
+                # If at beginning of the line alone
+                ch_width = 1
+            else:
+                ch_width = 0
+        elif re.match(variation_selector, string[i]):
+            if i == 0:
+                # If at beginning of the line alone
                 ch_width = 1
             else:
                 ch_width = 0
