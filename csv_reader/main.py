62cb1fb7da23d23512bfb47b5e881d8e363bdd99
mayone <mayone321@gmail.com>
2024-12-20 13:08:58 +0800

    [Change] Remove main_pure

--- csv_reader/main.py
+++ csv_reader/main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-import csv
-from CSV import CSV
 import config
+from CSV import CSV
 
 
 def main():
@@ -15,13 +14,11 @@ def main():
     print("input.csv")
     print(in_csv)
 
-    for row in in_csv.lines:
-        # Write header
-        if "Name" in row[0]:
-            fit_csv.write(row)
-            fat_csv.write(row)
-            continue
+    header = in_csv.lines[0]
+    fit_csv.write(header)
+    fat_csv.write(header)
 
+    for row in in_csv.lines[1:]:
         [name, height_cm, weight] = row
         height_m = float(height_cm) / 100
         weight = float(weight)
@@ -44,42 +41,6 @@ def main():
     in_csv.close()
     fit_csv.close()
     fat_csv.close()
-
-
-def main_pure():
-    in_file = open(config.INPUT_PATH, "r")
-    fit_out_file = open(config.OUTPUT_FIT_PATH, "w")
-    fat_out_file = open(config.OUTPUT_FAT_PATH, "w")
-
-    fit_writer = csv.writer(fit_out_file)
-    fat_writer = csv.writer(fat_out_file)
-
-    lines = csv.reader(in_file, delimiter=",")
-
-    overweight_bmi = 25
-
-    for row in lines:
-        # Write header
-        if "Name" in row[0]:
-            fit_writer.writerow(row)
-            fat_writer.writerow(row)
-            continue
-
-        [name, height_cm, weight] = row
-        height_m = float(height_cm) / 100
-        weight = float(weight)
-        bmi = (lambda h, w: w / (h * h))(height_m, weight)
-
-        if bmi < overweight_bmi:
-            print(f"{name}\t{bmi:.1f} -> fit")
-            fit_writer.writerow(row)
-        else:
-            print(f"{name}\t{bmi:.1f} -> fat")
-            fat_writer.writerow(row)
-
-    in_file.close()
-    fit_out_file.close()
-    fat_out_file.close()
 
 
 if __name__ == "__main__":
